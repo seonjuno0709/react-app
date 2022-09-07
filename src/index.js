181c647fd72070692ebae0a914fd0e1eb36c338d
Seon <76555222+seonjuno0709@users.noreply.github.com>
2022-09-07 09:46:49 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App />은  브라우저의 모든 것을 의미함. 삭제하면 인터페이스들이 사라짐 */}
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
